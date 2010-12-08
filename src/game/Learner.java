eb8129f7e3fa9fd7f5d027bf2e61856217e20d9c
comikal268 <comikal268@cb073d17-13f8-6a21-455c-6f8619f928e3>
2010-12-08 15:42:23 +0000

    Added in learner class. It uses the SmartRandomGuesser as a base for guessing and will use probability to generate further guesses.

---
+++ src/game/Learner.java
@@ -0,0 +1,145 @@
+/**
+ * 
+ */
+package game;
+
+import java.util.ArrayList;
+
+import data.CodeSequence;
+import data.ColorSpace;
+import data.Feedback;
+import data.RandomGuess;
+
+/**
+ * @author curtism2
+ * 
+ */
+public class Learner implements Guesser
+{
+
+	private final static int MAX_NR_GUESSES_TO_GENERATE = 25000;
+	private ColorSpace pegColors;
+	private int nrPegs;
+	private ArrayList<CodeSequence> guesses;
+	private ArrayList<CodeSequence> pastCodes;
+	private ArrayList<Feedback> feedbackForGuesses;
+	private int bestGuessIndex;
+	private ColorSpace workingColorSpace;
+
+	boolean guessMatchesAll;
+
+	public Learner(int nrPegs, int nrPegColors)
+	{
+		this.nrPegs = nrPegs;
+		pastCodes = new ArrayList<CodeSequence>();
+		pegColors = new ColorSpace(nrPegColors);
+		reset();
+	}
+
+	public void reset()
+	{
+		if (guesses != null)
+			study();
+		guesses = new ArrayList<CodeSequence>();
+		feedbackForGuesses = new ArrayList<Feedback>();
+		bestGuessIndex = -1;
+		workingColorSpace = new ColorSpace(pegColors);
+	}
+
+	private void study()
+	{
+		if (guesses.size() > 0)
+			System.out.println("Last guess " + guesses.get(guesses.size() - 1));
+
+	}
+
+	public CodeSequence guess()
+	{
+		if (guesses.size() != feedbackForGuesses.size())
+			throw new RuntimeException("Can't guess again until feedback is received for last guess");
+
+		CodeSequence guess = null;
+		int nrOfGuessMatches = 0;
+		CodeSequence bestNextGuess = null;
+		int nrOfBestNextGuessMatches = 0;
+		boolean guessMatchesAll = false;
+		int nrGuessesGenerated = 0;
+
+		while (!guessMatchesAll)
+		{
+			if (feedbackForGuesses.isEmpty()
+					|| feedbackForGuesses.get(bestGuessIndex).getBlack()
+							+ feedbackForGuesses.get(bestGuessIndex).getWhite() == 0)
+				guess = new RandomGuess(workingColorSpace, nrPegs);
+			else
+				guess = new RandomGuess(workingColorSpace,
+						guesses.get(bestGuessIndex),
+						feedbackForGuesses.get(bestGuessIndex));
+
+			nrOfGuessMatches = 0;
+
+			for (int i = 0; i < guesses.size(); i++)
+			{
+				if (guess.equals(guesses.get(i)))
+				{
+					nrOfGuessMatches = 0;
+					break;
+				}
+				if (guess.getFeedbackFor(guesses.get(i)).equals(
+						feedbackForGuesses.get(i)))
+					nrOfGuessMatches++;
+			}
+
+			nrGuessesGenerated++;
+
+			if (bestNextGuess == null
+					|| nrOfGuessMatches > nrOfBestNextGuessMatches)
+			{
+				nrOfBestNextGuessMatches = nrOfGuessMatches;
+				bestNextGuess = guess;
+			}
+
+			if (nrOfGuessMatches == guesses.size()
+					|| nrGuessesGenerated >= MAX_NR_GUESSES_TO_GENERATE)
+				guessMatchesAll = true;
+
+		}
+
+		System.out.println(nrGuessesGenerated
+				+ " guesses generated-- best guess matches "
+				+ ((guesses.size() == nrOfGuessMatches) ? "all "
+						+ guesses.size() + " previous" : nrOfGuessMatches
+						+ " of " + guesses.size()) + " guesses...");
+		guesses.add(bestNextGuess);
+		return bestNextGuess;
+	}
+
+	public void giveFeedbackForLastGuess(Feedback feedback)
+	{
+		if (feedbackForGuesses.size() != guesses.size() - 1)
+			throw new RuntimeException("Already gave feedback for last guess");
+
+		feedbackForGuesses.add(feedback);
+
+		CodeSequence lastGuess = guesses.get(guesses.size() - 1);
+		if (feedback.getBlack() + feedback.getWhite() == 0)
+			for (int i = 0; i < lastGuess.getNrPegs(); i++)
+				workingColorSpace.removeColor(lastGuess.getPegColorAt(i));
+
+		if (bestGuessIndex < 0
+				|| feedback.getValue() > feedbackForGuesses.get(bestGuessIndex)
+						.getValue())
+			bestGuessIndex = feedbackForGuesses.size() - 1;
+	}
+
+	public String toString()
+	{
+		String returnString = "";
+		for (int i = 0; i < feedbackForGuesses.size(); i++)
+		{
+			returnString += "GUESS: " + guesses.get(i) + "\tFEEDBACK:"
+					+ feedbackForGuesses.get(i) + "\n";
+		}
+		return returnString;
+	}
+}
--- src/testing/Tester.java
+++ src/testing/Tester.java
@@ -1,159 +1,177 @@
 package testing;
 
+import game.Guesser;
+import game.KnuthGuesser;
+import game.Learner;
+import game.Oracle;
+import game.SmartRandomGuesser;
+
 import java.util.Scanner;
 
-import game.Guesser;
-import game.SmartRandomGuesser;
-import game.KnuthGuesser;
-import game.Oracle;
 import data.CodeSequence;
 import data.Feedback;
 
-public class Tester 
+public class Tester
 {
-        private static final String VERBOSITY_ON = "-v";
-        private static final String USE_KNUTH_4X6 = "-k";
-        private static final String NR_PEGS = "-p";
-        private static final String NR_COLORS = "-c";
-        private static final String NR_GAMES = "-g";
-        
-        private static Scanner in = null;
-        private static String fileName = null;
-        private static boolean verbose = false;
-        private static boolean knuth = false;
-        private static int nrPegs = 0;
-        private static int nrColors = 0;
-        private static int nrGames = 0;
-        
-        private static void verifyArgsAndSetup( String[] args )
-        {
-                for( int i = 0; i < args.length; i++ )
-                        if( args[i].equals( VERBOSITY_ON ) )
-                                verbose = true;
-                        else if( args[i].equals( USE_KNUTH_4X6 ) )
-                        		knuth = true;
-                        else if( args[i].equals( NR_PEGS ) )
-                                nrPegs = Integer.parseInt(args[++i]);
-                        else if( args[i].equals( NR_COLORS ) )
-                                nrColors = Integer.parseInt(args[++i]);
-                        else if( args[i].equals( NR_GAMES ) )
-                                nrGames = Integer.parseInt(args[++i]);
-                        else if( fileName == null )
-                                fileName = args[i];
-                        else
-                                System.out.println( "Unknown command line argument - \"" + args[i] + "\"" );
-                
-                if( fileName == null )
-                        in = new Scanner( System.in );
-        }
-        
-        private static Oracle initOracle()
-        {
-                if( fileName != null )
-                {
-                	if( knuth && !(nrPegs == 4 && nrColors == 6))
-                    {
-                    	knuth = false;
-                    }
-                	
-                	return new Oracle( fileName );
-                }
-                
-                if( knuth )
-                {
-                	nrPegs = 4;
-                	nrColors = 6;
-                }
-                else
-                {
-                	if( nrPegs <= 0 )
-                	{
-                        System.out.print( "Enter number of pegs for mastermind: " );
-                        nrPegs = in.nextInt(); in.nextLine();
-                	}
-                
-                	if( nrColors <= 0 )
-                	{
-                		System.out.print( "Enter number of peg colors for mastermind: " );
-                        nrColors = in.nextInt(); in.nextLine();
-                	}
-                	
-                	System.out.println( "" );
-                }
-                
-                if( nrGames <= 0 )
-                	nrGames = 1;
-                        
-                return new Oracle( nrPegs, nrColors );
-        }
-        
-        public static void main( String[] args )
-        {
-                verifyArgsAndSetup(args);
-                Oracle oracle = initOracle();
-                Guesser guesser;
-                
-                if( knuth )
-                	guesser = new KnuthGuesser();
-                else
-                	guesser = new SmartRandomGuesser(oracle.getNumPegs(),oracle.getNumPegColors());
-                                
-                int nrGamesPlayed = 0;
-                int nrGuessesTotal = 0;
-                int nrGuessesPerGame;
-                int maxNrGuessesForOneGame = 0;
-                int nrGamesAtMaxGuesses = 0;
-                
-                while(oracle.hasCodeToUseFromFile() || nrGamesPlayed < nrGames) 
-                {
-                        nrGuessesPerGame = 0;
-                        
-                        if(verbose)
-                                System.out.println( "\n----------------------------------------------" );
-                        
-                        while(true)
-                        {
-                                CodeSequence guess = guesser.guess();
-                                Feedback feedback = oracle.getFeedbackFor(guess);
-                                guesser.giveFeedbackForLastGuess(feedback);
-                                nrGuessesPerGame++;
-                        
-                                if(verbose)
-                                {
-                                        System.out.print( "GUESS: " + guess );
-                                        System.out.println( "\tFEEDBACK:" + feedback + "\n" );
-                                }
-                        
-                                if(feedback.getBlack() == oracle.getNumPegs())
-                                        break;
-                        }
-                        nrGamesPlayed++;
-                        nrGuessesTotal += nrGuessesPerGame;
-                        if( nrGuessesPerGame > maxNrGuessesForOneGame )
-                        {
-                                maxNrGuessesForOneGame = nrGuessesPerGame;
-                                nrGamesAtMaxGuesses = 1;
-                                //System.out.println( guesser );
-                        }
-                        else if( nrGuessesPerGame == maxNrGuessesForOneGame )
-                        {
-                                nrGamesAtMaxGuesses++;
-                        }
-                        
-                        if(verbose)
-                        {
-                                System.out.println( "\nSOLVED GAME #" + nrGamesPlayed + " IN "+ nrGuessesPerGame + " GUESSES!" );
-                                System.out.println( "----------------------------------------------\n" );
-                        }
-                        
-                        oracle.generateNextCode();
-                        guesser.reset();
-                }
-                
-                System.out.println( "Played " + nrGamesPlayed + " game" + (nrGamesPlayed > 1 ? "s" : "") + " with " + oracle.getNumPegs() + 
-                                                        " PEGS and " + oracle.getNumPegColors() + " COLORS..." );
-                System.out.println( "\tAVERGAGE NUMBER OF GUESSES: " + nrGuessesTotal / (float)nrGamesPlayed );
-                System.out.println( "\t MAXIMUM NUMBER OF GUESSES: " + maxNrGuessesForOneGame );
-                System.out.println( "\t   PERCENT OF GAMES AT MAX: " + nrGamesAtMaxGuesses / (float)nrGamesPlayed );
-        }
+	private static final String VERBOSITY_ON = "-v";
+	private static final String USE_KNUTH_4X6 = "-k";
+	private static final String LEARNING = "-l";
+	private static final String NR_PEGS = "-p";
+	private static final String NR_COLORS = "-c";
+	private static final String NR_GAMES = "-g";
+
+	private static Scanner in = null;
+	private static String fileName = null;
+	private static boolean verbose = false;
+	private static boolean knuth = false;
+	private static boolean learn = false;
+	private static int nrPegs = 0;
+	private static int nrColors = 0;
+	private static int nrGames = 0;
+
+	private static void verifyArgsAndSetup(String[] args)
+	{
+		for (int i = 0; i < args.length; i++)
+			if (args[i].equals(VERBOSITY_ON))
+				verbose = true;
+			else if (args[i].equals(USE_KNUTH_4X6))
+				knuth = true;
+			else if (args[i].equals(LEARNING))
+				learn = true;
+			else if (args[i].equals(NR_PEGS))
+				nrPegs = Integer.parseInt(args[++i]);
+			else if (args[i].equals(NR_COLORS))
+				nrColors = Integer.parseInt(args[++i]);
+			else if (args[i].equals(NR_GAMES))
+				nrGames = Integer.parseInt(args[++i]);
+			else if (fileName == null)
+				fileName = args[i];
+			else
+				System.out.println("Unknown command line argument - \""
+						+ args[i] + "\"");
+
+		if (fileName == null)
+			in = new Scanner(System.in);
+	}
+
+	private static Oracle initOracle()
+	{
+		if (fileName != null)
+		{
+			if (knuth && !(nrPegs == 4 && nrColors == 6))
+			{
+				knuth = false;
+			}
+
+			return new Oracle(fileName);
+		}
+
+		if (knuth)
+		{
+			nrPegs = 4;
+			nrColors = 6;
+		}
+		else
+		{
+			if (nrPegs <= 0)
+			{
+				System.out.print("Enter number of pegs for mastermind: ");
+				nrPegs = in.nextInt();
+				in.nextLine();
+			}
+
+			if (nrColors <= 0)
+			{
+				System.out.print("Enter number of peg colors for mastermind: ");
+				nrColors = in.nextInt();
+				in.nextLine();
+			}
+
+			System.out.println("");
+		}
+
+		if (nrGames <= 0)
+			nrGames = 1;
+
+		return new Oracle(nrPegs, nrColors);
+	}
+
+	public static void main(String[] args)
+	{
+		verifyArgsAndSetup(args);
+		Oracle oracle = initOracle();
+		Guesser guesser;
+
+		if (knuth)
+			guesser = new KnuthGuesser();
+		else if (learn)
+			guesser = new Learner(oracle.getNumPegs(), oracle.getNumPegColors());
+		else
+			guesser = new SmartRandomGuesser(oracle.getNumPegs(),
+					oracle.getNumPegColors());
+
+		int nrGamesPlayed = 0;
+		int nrGuessesTotal = 0;
+		int nrGuessesPerGame;
+		int maxNrGuessesForOneGame = 0;
+		int nrGamesAtMaxGuesses = 0;
+
+		while (oracle.hasCodeToUseFromFile() || nrGamesPlayed < nrGames)
+		{
+			nrGuessesPerGame = 0;
+
+			if (verbose)
+				System.out.println("\n----------------------------------------------");
+
+			while (true)
+			{
+				CodeSequence guess = guesser.guess();
+				Feedback feedback = oracle.getFeedbackFor(guess);
+				guesser.giveFeedbackForLastGuess(feedback);
+				nrGuessesPerGame++;
+
+				if (verbose)
+				{
+					System.out.print("GUESS: " + guess);
+					System.out.println("\tFEEDBACK:" + feedback + "\n");
+				}
+
+				if (feedback.getBlack() == oracle.getNumPegs())
+					break;
+			}
+			nrGamesPlayed++;
+			nrGuessesTotal += nrGuessesPerGame;
+			if (nrGuessesPerGame > maxNrGuessesForOneGame)
+			{
+				maxNrGuessesForOneGame = nrGuessesPerGame;
+				nrGamesAtMaxGuesses = 1;
+				// System.out.println( guesser );
+			}
+			else if (nrGuessesPerGame == maxNrGuessesForOneGame)
+			{
+				nrGamesAtMaxGuesses++;
+			}
+
+			if (verbose)
+			{
+				System.out.println("\nSOLVED GAME #" + nrGamesPlayed + " IN "
+						+ nrGuessesPerGame + " GUESSES!");
+				System.out.println("----------------------------------------------\n");
+			}
+
+			oracle.generateNextCode();
+			guesser.reset();
+		}
+
+		System.out.println("Played " + nrGamesPlayed + " game"
+				+ (nrGamesPlayed > 1 ? "s" : "") + " with "
+				+ oracle.getNumPegs() + " PEGS and " + oracle.getNumPegColors()
+				+ " COLORS...");
+		System.out.println("\tAVERGAGE NUMBER OF GUESSES: " + nrGuessesTotal
+				/ (float) nrGamesPlayed);
+		System.out.println("\t MAXIMUM NUMBER OF GUESSES: "
+				+ maxNrGuessesForOneGame);
+		System.out.println("\t   PERCENT OF GAMES AT MAX: "
+				+ nrGamesAtMaxGuesses / (float) nrGamesPlayed);
+	}
 }
