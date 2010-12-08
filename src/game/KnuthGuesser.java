a60fd23fd21ec2d069f98ddb3c2d7de7967dfc5e
brenfar1 <brenfar1@cb073d17-13f8-6a21-455c-6f8619f928e3>
2010-12-08 01:30:08 +0000

    Added Knuth's 5 guess algorithm which can be invoked with the "-k" command line argument.  This will only work with a code file, if the codes are 4X6.  Using "-k" will ensure that the games are 4X6 regardless of the "-p" and "-c" specifications.

---
+++ src/game/KnuthGuesser.java
@@ -0,0 +1,157 @@
+package game;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import data.CodeSequence;
+import data.Feedback;
+
+public class KnuthGuesser implements Guesser
+{
+	private static final int GUESS_SPACE_SIZE = 1296;
+	private static final int FEEDBACK_SPACE_SIZE = 14;
+	
+	private ArrayList<CodeSequence> currentGuessSpace;
+	private ArrayList<CodeSequence> entireGuessSpace;
+	private ArrayList<Feedback> feedbackSpace;
+	private CodeSequence lastGuess;
+	private Feedback lastFeedback;
+	
+	public KnuthGuesser()
+	{
+		reset();
+		entireGuessSpace = createGuessSpace();
+		feedbackSpace = createFeedbackSpace();
+	}
+	
+	public void giveFeedbackForLastGuess(Feedback feedback) 
+	{
+		lastFeedback = feedback;
+	}
+
+	
+	public CodeSequence guess() 
+	{
+		if( lastGuess == null )
+		{
+			lastGuess = new CodeSequence( new char[]{'A', 'A', 'B', 'C' } );
+			return lastGuess;
+		}
+				
+		removeImpossibleGuessesFromGuessSpace();
+		
+		if( currentGuessSpace.size() == 1 )
+			return currentGuessSpace.get( 0 );
+		
+		CodeSequence bestGuess = null;
+		int bestSpaceReduction = 0;
+		int currentLowestReductionForGuess;
+		int reductionForGuess;
+		
+		for( int i = 0; i < entireGuessSpace.size(); i++ )
+		{
+			currentLowestReductionForGuess = GUESS_SPACE_SIZE;
+			for( int j = 0; j < feedbackSpace.size(); j++ )
+			{
+				reductionForGuess = getCountOfImpossibleGuesses( entireGuessSpace.get(i), feedbackSpace.get(j) );
+
+				if( reductionForGuess < currentLowestReductionForGuess )
+				{
+					currentLowestReductionForGuess = reductionForGuess;
+				}
+			}
+			
+			if( bestGuess == null || currentLowestReductionForGuess > bestSpaceReduction )
+			{
+				bestGuess = entireGuessSpace.get(i);
+				bestSpaceReduction = currentLowestReductionForGuess;
+			}
+		}
+		
+		lastGuess = bestGuess;
+		return bestGuess;
+	}
+
+	private void removeImpossibleGuessesFromGuessSpace()
+	{
+		Iterator<CodeSequence> itr = currentGuessSpace.listIterator();
+		CodeSequence currentCode = null;
+		
+		while( itr.hasNext() )
+		{
+			currentCode = itr.next();
+			if( !currentCode.getFeedbackFor( lastGuess ).equals( lastFeedback ) )
+				itr.remove();
+		}
+	}
+	
+	private int getCountOfImpossibleGuesses( CodeSequence guess, Feedback feedback )
+	{
+		int nrOfImpossibleGuesses = 0;
+		Iterator<CodeSequence> itr = currentGuessSpace.listIterator();
+		CodeSequence currentCode = null;
+		
+		while( itr.hasNext() )
+		{
+			currentCode = itr.next();
+			if( !currentCode.getFeedbackFor( guess ).equals( feedback ) )
+			{
+				nrOfImpossibleGuesses++;
+			}
+		}
+		
+		return nrOfImpossibleGuesses;
+	}
+	
+	public void reset() 
+	{
+		currentGuessSpace = createGuessSpace();
+		lastGuess = null;
+		lastFeedback = null;
+	}
+	
+	private ArrayList<CodeSequence> createGuessSpace()
+	{
+		ArrayList<CodeSequence> guessSpace = new ArrayList<CodeSequence>();
+		
+		char[] code = {'A', 'A', 'A', 'A' };
+		for( int i = 0; i < GUESS_SPACE_SIZE; i++ )
+		{
+			guessSpace.add(new CodeSequence(code));
+			for( int j = 3; j >= 0; j-- )
+			{
+				if( code[j] == 'F' )
+				{
+					code[j] = 'A';
+				}
+				else
+				{
+					code[j]++;
+					break;
+				}
+			}
+		}
+		
+		return guessSpace;
+	}
+	
+	private ArrayList<Feedback> createFeedbackSpace()
+	{
+		ArrayList<Feedback> feedbackSpace = new ArrayList<Feedback>();
+		
+		int whiteCount = 0;
+		int blackCount = 0;
+		for( int i = 0; i < FEEDBACK_SPACE_SIZE; i++ )
+		{
+			feedbackSpace.add( new Feedback( whiteCount, blackCount ) );
+			blackCount++;
+			if( blackCount + whiteCount > 4 || (blackCount == 3 && whiteCount == 1) )
+			{
+				blackCount = 0;
+				whiteCount++;
+			}
+		}
+		
+		return feedbackSpace;
+	}
+}
---
+++ src/game/SmartRandomGuesser.java
@@ -0,0 +1,110 @@
+package game;
+
+import java.util.ArrayList;
+
+import data.CodeSequence;
+import data.Feedback;
+import data.RandomGuess;
+
+public class SmartRandomGuesser implements Guesser 
+{
+        private String pegColors;
+        private int nrPegs;
+        private ArrayList<CodeSequence> guesses;
+        private ArrayList<Feedback> feedbackForGuesses;
+        private int bestGuessIndex;
+        private String workingColorSpace;
+        
+        boolean guessMatchesAll;
+        
+        public SmartRandomGuesser(int nrPegs, int nrPegColors)
+        {
+                this.nrPegs = nrPegs;
+                populatePegColors(nrPegColors);
+                reset();
+        }
+        
+        public void reset()
+        {
+                guesses = new ArrayList<CodeSequence>();
+                feedbackForGuesses = new ArrayList<Feedback>();
+                bestGuessIndex = -1;
+                workingColorSpace = pegColors;
+        }
+        
+        private void populatePegColors(int nrPegColors)
+        {
+                pegColors = "";
+                for(int i = 0; i < nrPegColors; i++)
+                        pegColors += (char)('A' + i) + "";
+        }
+        
+        public CodeSequence guess()
+    	{
+    		if( guesses.size() != feedbackForGuesses.size() )
+    			throw new RuntimeException("Can't guess again until feedback is received for last guess");
+    		
+    		CodeSequence guess = null;
+    		CodeSequence lastGuess = null;
+    		Feedback lastFeedback = null;
+    		boolean guessMatchesAll;
+    		
+    		if (guesses.size() == 0 )
+    			guess = new RandomGuess( pegColors, nrPegs );
+    		else
+    		{	
+    			guessMatchesAll = false;
+    			while(!guessMatchesAll) 
+    			{ 
+    				lastFeedback = feedbackForGuesses.get(feedbackForGuesses.size() - 1);
+    				lastGuess = guesses.get(guesses.size() - 1 );
+    				
+    				if( lastFeedback.getBlack() + lastFeedback.getWhite() == nrPegs )				
+    					guess = new RandomGuess(lastGuess);
+    				else if( feedbackForGuesses.size() < 1 || 
+    						(feedbackForGuesses.get(bestGuessIndex).getBlack() + feedbackForGuesses.get(bestGuessIndex).getWhite() == 0 ) )
+    					guess = new RandomGuess( workingColorSpace, nrPegs );
+    				else
+    					guess = new RandomGuess( workingColorSpace, guesses.get(bestGuessIndex), feedbackForGuesses.get(bestGuessIndex) );
+    		
+    				int i;
+    				for(i = 0; i < guesses.size(); i++ )
+    					if( guess.equals( guesses.get(i) ) ||
+    							( //( nrPegs < 8 || lastFeedback.getBlack() + lastFeedback.getWhite() == nrPegs || i == guesses.size() - 1 ) &&
+    									!guess.getFeedbackFor( guesses.get(i) ).equals( feedbackForGuesses.get(i) ) ) )
+    						break;
+    				if( i == guesses.size() )
+    					guessMatchesAll = true;
+    			}
+    		}
+    			
+    		guesses.add(guess);
+    		return guess;
+    	}
+        
+        public void giveFeedbackForLastGuess(Feedback feedback)
+        {
+                if(feedbackForGuesses.size() != guesses.size() - 1)
+                        throw new RuntimeException("Already gave feedback for last guess");
+                
+                feedbackForGuesses.add(feedback);
+                
+                CodeSequence lastGuess = guesses.get(guesses.size() - 1);
+                if(feedback.getBlack() + feedback.getWhite() == 0)
+                        for( int i = 0; i < lastGuess.getNrPegs(); i++ ) 
+                                workingColorSpace = workingColorSpace.replace(lastGuess.getPegColorAt(i) + "", "");
+                
+                if(bestGuessIndex < 0 || feedback.getValue() > feedbackForGuesses.get(bestGuessIndex).getValue())
+                        bestGuessIndex = feedbackForGuesses.size() - 1;
+        }
+        
+        public String toString()
+        {
+                String returnString = "";
+                for( int i = 0; i < feedbackForGuesses.size(); i++ )
+                {
+                        returnString += "GUESS: " + guesses.get(i) + "\tFEEDBACK:" + feedbackForGuesses.get(i) + "\n";
+                }
+                return returnString;
+        }
+}
