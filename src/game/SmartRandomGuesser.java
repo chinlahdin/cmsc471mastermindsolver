c290e9065e91709a92a0b63c864d89bf08e72ee4
brenfar1 <brenfar1@cb073d17-13f8-6a21-455c-6f8619f928e3>
2010-12-08 07:25:34 +0000

    I put a limit on the number of generated guesses the SmartRandomGuesser generates before submitting a guess.  The best guess (guess which matches feedback with the most previous guesses) is what is submitted.  We can play with this limit to try and find a balnce between number of average guesses per game and CPU time.  I tried it at 100,000 and was able to solve 10 15X15 games, but it took about 15 minutes.
    Output from that:
    .
    .
    .
    100000 guesses generated...
    GUESS: 6 14 4 14 12 3 12 13 12 2 10 10 5 8 15	FEEDBACK:bbbbbbbbbbwwwww
    100000 guesses generated...
    GUESS: 3 14 15 14 12 8 12 13 12 2 10 10 5 6 4	FEEDBACK:bbbbbbbbbbwwwww
    4001 guesses generated...
    GUESS: 6 14 15 14 12 4 12 13 12 2 3 10 5 10 8	FEEDBACK:bbbbbbbbbbbbbbb
    
    SOLVED GAME #10 IN 31 GUESSES!
    ----------------------------------------------
    
    Played 10 games with 15 PEGS and 15 COLORS...
    	AVERGAGE NUMBER OF GUESSES: 28.4
    	 MAXIMUM NUMBER OF GUESSES: 37
    	   PERCENT OF GAMES AT MAX: 0.1

--- src/game/SmartRandomGuesser.java
+++ src/game/SmartRandomGuesser.java
@@ -9,6 +9,8 @@ import data.RandomGuess;
 
 public class SmartRandomGuesser implements Guesser 
 {
+		private final static int MAX_NR_GUESSES_TO_GENERATE = 100000;
+	
         private ColorSpace pegColors;
         private int nrPegs;
         private ArrayList<CodeSequence> guesses;
@@ -39,6 +41,9 @@ public class SmartRandomGuesser implements Guesser
     			throw new RuntimeException("Can't guess again until feedback is received for last guess");
     		
     		CodeSequence guess = null;
+    		int nrOfGuessMatches = 0;
+    		CodeSequence bestNextGuess = null;
+    		int nrOfBestNextGuessMatches = 0;
     		boolean guessMatchesAll = false;
     		int nrGuessesGenerated = 0;
     		
@@ -49,18 +54,28 @@ public class SmartRandomGuesser implements Guesser
    				else
     				guess = new RandomGuess( workingColorSpace, guesses.get(bestGuessIndex), feedbackForGuesses.get(bestGuessIndex) );
     		
-   				int i;
-   				for(i = 0; i < guesses.size(); i++ )
-   					if( guess.equals( guesses.get(i) ) || !guess.getFeedbackFor( guesses.get(i) ).equals( feedbackForGuesses.get(i) ) )
-   						break;
-   				if( i == guesses.size() )
-   					guessMatchesAll = true;
+   				nrOfGuessMatches = 0;
+   				
+   				for(int i = 0; i < guesses.size(); i++ )
+   					if( !guess.equals( guesses.get(i) ) && guess.getFeedbackFor( guesses.get(i) ).equals( feedbackForGuesses.get(i) ) )
+   						nrOfGuessMatches++;
+   				
    				nrGuessesGenerated++;
+   				
+   				if( bestNextGuess == null || nrOfGuessMatches > nrOfBestNextGuessMatches )
+   				{
+   					nrOfBestNextGuessMatches = nrOfGuessMatches;
+   					bestNextGuess = guess;
+   				}
+   				
+   				if( nrOfGuessMatches == guesses.size() || nrGuessesGenerated >= MAX_NR_GUESSES_TO_GENERATE )
+   					guessMatchesAll = true;
+   				
    			}
  
    			System.out.println( nrGuessesGenerated + " guesses generated..." );
-    		guesses.add(guess);
-    		return guess;
+    		guesses.add(bestNextGuess);
+    		return bestNextGuess;
     	}
         
         public void giveFeedbackForLastGuess(Feedback feedback)
