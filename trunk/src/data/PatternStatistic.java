9ccc95f635fbb0bc7f11d1b2b408358a49e7f72b
comikal268 <comikal268@cb073d17-13f8-6a21-455c-6f8619f928e3>
2010-12-09 00:04:53 +0000

    Learning now incorporates codes from experience. All codes and substrings of codes are analyzed.

--- trunk/src/data/PatternStatistic.java
+++ trunk/src/data/PatternStatistic.java
@@ -1,33 +1,53 @@
 package data;
 
-public class ColorStatistic 
+public class PatternStatistic
 {
 	private int count;
 	private double probability;
 	private String colorID;
-	
-	public ColorStatistic(String id)
+
+	public PatternStatistic(String id)
 	{
 		colorID = id;
 		count = 0;
 		probability = 0.0;
 	}
-	
+
 	public int getCount()
 	{
 		return this.count;
 	}
+
+	public String getRegex()
+	{
+		return colorID;
+	}
+
 	public double getProbability(int instances)
 	{
 		probability = (1.0 * count) / (1.0 * instances);
 		return this.probability;
 	}
+
 	public void increaseCount()
 	{
 		this.count++;
 	}
+
 	public String toString()
 	{
-		return ""+colorID + ": ("+ count + ")---"+probability;
+		return "" + colorID + ": (" + count + ")---" + probability;
+	}
+
+	public boolean equals(Object o)
+	{
+		if (o.getClass().equals(this.getClass()))
+		{
+			return ((PatternStatistic) o).getRegex().equalsIgnoreCase(
+					this.getRegex()) ? true : false;
+
+		}
+		return false;
+
 	}
 }
